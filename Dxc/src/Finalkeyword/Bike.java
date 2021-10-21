a1da294a54ba03555106469d6dbc51fdcaae974e
HP <HP@LAPTOP-186DP2UE>
2021-10-21 11:34:18 +0530

    Finalkeyword

---
+++ Dxc/src/Finalkeyword/Bike.java
@@ -0,0 +1,15 @@
+package Finalkeyword;
+
+public class Bike {
+ 
+	  final int speedlimit;//blank final variable  
+	    
+	  Bike(){  
+	  speedlimit=70;  
+	  System.out.println(speedlimit);  
+	  }  
+	  
+	  public static void main(String args[]){  
+	    new Bike();  
+	 }  
+	}  
---
+++ Dxc/src/Finalkeyword/Student.java
@@ -0,0 +1,8 @@
+package Finalkeyword;
+
+public class Student { 
+int id;  
+String name;  
+final String PAN_CARD_NUMBER;  
+...  
+}  
