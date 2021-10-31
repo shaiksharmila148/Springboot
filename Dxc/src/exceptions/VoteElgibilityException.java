706e0e3a1b014b2bd70167d6cd7f3ab73e540df1
HP <HP@LAPTOP-186DP2UE>
2021-10-31 11:45:25 +0530

    exceptions

---
+++ Dxc/src/exceptions/VoteElgibilityException.java
@@ -0,0 +1,10 @@
+package exceptions;
+public class VoteElgibilityException extends Exception{
+
+	public VoteElgibilityException(String message) {
+		super("ineligible to vote this year");
+	}
+	
+	
+
+}
