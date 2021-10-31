4d9d649cbbdc1a63bdbf5cfe160072bd23b57437
HP <HP@LAPTOP-186DP2UE>
2021-10-31 15:46:00 +0530

    polymorphism

---
+++ Dxc/src/polymorphism/Animal.java
@@ -0,0 +1,12 @@
+package polymorphism;
+
+
+class Animal{  
+	Animal(){System.out.println("animal is created");}  
+}  
+class Dog extends Animal{  
+	Dog(){  
+		//super();  
+		System.out.println("dog is created");  
+	}  
+}  
